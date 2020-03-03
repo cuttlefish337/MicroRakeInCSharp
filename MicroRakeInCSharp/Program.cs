9faea425d0a6d967722318ff9db60aa85ebeb2e8
Arthur <cuttlefish337@gmail.com>
2020-03-03 17:45:50 +0100

    It works

--- MicroRakeInCSharp/Program.cs
+++ MicroRakeInCSharp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 
+// source: https://www.youtube.com/watch?v=0D3KfnbTdWw
+
 namespace MicroRakeInCSharp
 {
     public static class MicroRake
@@ -45,17 +47,17 @@ namespace MicroRakeInCSharp
     }
     class Program
     {
-        /*
-            make mac and cheese
-            boil water
-            buy pasta
-            buy cheese
-            goto the store
-        */
+        
         static void Main(string[] args)
         {
+            MicroRake.AddTask("buy_cheese", new MRTask("buy_cheese", new string[] {"goto_the_store"}, () => Console.WriteLine("Buy cheese.")));
             MicroRake.AddTask("goto_the_store", new MRTask("goto_the_store", new string[] {}, () => Console.WriteLine("Goto the store.")));
-            MicroRake.AddTask("boil_water", new MRTask("boil_water", new string[] {}))
+            MicroRake.AddTask("boil_water", new MRTask("boil_water", new string[] {"buy_cheese", "buy_pasta"}, () => Console.WriteLine("Boil water.")));
+            MicroRake.AddTask("buy_pasta", new MRTask("buy_pasta", new string[] {"goto_the_store"}, () => Console.WriteLine("Buy pasta.")));
+            MicroRake.AddTask("make_mac_and_cheese", new MRTask("make_mac_and_cheese", new string[] {"buy_pasta", "buy_cheese", "boil_water"}, () => Console.WriteLine("Make mac and cheese.")));
+            
+            // calculate dependencies
+            MicroRake.AllTasks["make_mac_and_cheese"].MRInvoke();
         }
     }
 }
