148fcf129e79338f4e9fd26330e5243d83c94932
Arthur <cuttlefish337@gmail.com>
2020-03-03 17:18:08 +0100

    Microrake

--- MicroRakeInCSharp/Program.cs
+++ MicroRakeInCSharp/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MicroRakeInCSharp
 {
+    public static class MicroRake
+    {
+        public static Dictionary<string, MRTask> AllTasks = new Dictionary<string, MRTask>();
+
+        public static void AddTask(string name, MRTask mrtask)
+        {
+            AllTasks.Add(name, mrtask);
+
+        }
+    }
+
+    public class MRTask
+    {
+        string Name;
+        string[] Deps;
+        Action MRAction;
+        bool AlreadyRun;
+        public MRTask(string name, string[] deps, Action mraction)
+        {
+            Name = name;
+            Deps = deps;
+            MRAction = mraction;
+            AlreadyRun = false;
+        }
+
+        public void MRInvoke()
+        {
+            if (AlreadyRun == true)
+                return;
+            foreach(string dep in Deps)
+                MicroRake.AllTasks[dep].MRInvoke();
+            Execute();
+            AlreadyRun = true;
+        }
+
+        public void Execute()
+        {
+            MRAction.Invoke();
+        }
+    }
     class Program
     {
+        /*
+            make mac and cheese
+            boil water
+            buy pasta
+            buy cheese
+            goto the store
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MicroRake.AddTask("goto_the_store", new MRTask("goto_the_store", new string[] {}, () => Console.WriteLine("Goto the store.")));
+            MicroRake.AddTask("boil_water", new MRTask("boil_water", new string[] {}))
         }
     }
 }
