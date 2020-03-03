e1be20575fb5831f1ae47f1ac4b54596e75db0ce
Arthur <cuttlefish337@gmail.com>
2020-03-03 15:12:49 +0100

    Initial commit

---
+++ MicroRakeInCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroRakeInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
